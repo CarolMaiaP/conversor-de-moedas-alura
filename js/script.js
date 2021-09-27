aa05edd73471cb1b385a5f93e29c7b47ba5f24ab
Caroline Maia <carolmas99@gmail.com>
2021-09-27 18:39:58 -0300

    sending project logic

---
+++ js/script.js
@@ -0,0 +1,13 @@
+function Converter() {
+  var valorElemento = document.getElementById("valor");
+  var valor = valorElemento.value;
+  var valorEmDolarNumerico = parseFloat(valor);
+
+  var valorEmReal = valorEmDolarNumerico * 5;
+  console.log(valorEmReal);
+
+  var elementoValorConvertido = document.getElementById("valorConvertido");
+  var valorConvertido = "O resultado em Real é R$ " + valorEmReal;
+
+  elementoValorConvertido.innerHTML = valorConvertido;
+}
